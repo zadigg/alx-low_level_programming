e771059d7c13991e9150db63964fd93581e24e77
abelkibebe5@gmail.com <zadigg>
2022-04-07 06:27:14 -0700

    Write a C program that prints exactly

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ * main - entry point
+ * @void: no arguments
+ * Description: Programming is like building a multilingual puzzle.
+ * Return: value 0 to exit
+ */
+#include <stdio.h>
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
