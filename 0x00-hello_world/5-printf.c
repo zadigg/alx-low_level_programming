049a0321b5ee55d436656d9701078010252f7f4f
abelkibebe5@gmail.com <zadigg>
2022-04-07 06:31:41 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - entry point
+ * @void: no arguments
+ * Description: Programming is like building a multilingual puzzle.
+ * Return: value 0 to exit
+ */
+#include <stdio.h>
+int main()
+{
+    printf("with proper grammar, but the outcome is a piece of art, \n"); 
+    return 0;
+}
