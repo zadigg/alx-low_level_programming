343fa1d224f383ac554fdcde9b505dd132274a23
zadigg <abelkibebe5@gmail.com>
2022-07-29 23:04:59 +0300

    Fixed 0x00-hello_world/README.md ✔️✔️

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,32 +1,26 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Create a hash table
- * @size: size of the array
- * Return: new hash table or NULL if error
+ * hash_table_create - creates a hash table
+ * @size: size of the array of the hash table
+ *
+ * Return: Pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hsh;
-	unsigned long int i;
+	hash_table_t *ht;
 
 	if (size == 0)
 		return (NULL);
 
-	hsh = malloc(sizeof(hash_table_t));
-	if (hsh == NULL)
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
 		return (NULL);
 
-	hsh->array = malloc(sizeof(hash_node_t *) * size);
-	if (hsh->array == NULL)
-	{
-		free(hsh);
+	ht->size = size;
+	ht->array = calloc((size_t)ht->size, sizeof(hash_node_t *));
+	if (ht->array == NULL)
 		return (NULL);
-	}
-	hsh->size = size;
 
-	for (i = 0; i < size; i++)
-		hsh->array[i] = NULL;
-
-	return (hsh);
+	return (ht);
 }
--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,12 +1,21 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Gives the index of a key
- * @key: key of key/value pair
- * @size: size of array of the hash table
- * Return: index where key/value pair should be stored in array of hash table
+ * key_index - gives the index of a key in a hash table
+ * @key: the key to find the index of
+ * @size: size of the array of indexes
+ *
+ * Return: the index at which the key/value pair
+ * should be stored in the array of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) % size);
+	unsigned long int hash;
+
+	if (size == 0)
+		return (0);
+
+	hash = hash_djb2(key);
+
+	return (hash % size);
 }
